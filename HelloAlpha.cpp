664b0d9c4d5d035d90353ccbdb4985f15ed17c13
Alpha768 <375564365@qq.com>
2019-08-24 12:27:34 +0800

    Add File

---
+++ HelloAlpha.cpp
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+extern void TestFun();
+int main(int argc,char **argv)
+{
+    printf("Hello World\n");
+    TestFun();
+    getchar();
+    return 0;
+}
+
+
+
---
+++ TestFun.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void TestFun()
+{
+    printf("Test Func\n");
+}
+
+
