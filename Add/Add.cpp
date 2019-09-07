bc567351e12034fc1dc6ea7117bc62ccddc5b7df
Alpha768 <375564365@qq.com>
2019-09-07 12:48:35 +0800

    add  fold add

---
+++ Add/Add.cpp
@@ -0,0 +1,5 @@
+#include <stdio.h>
+void AddFunc(void)
+{
+    printf("Hello Add Function\n");
+}
--- HelloAlpha.cpp
+++ HelloAlpha.cpp
@@ -1,10 +1,12 @@
 
 #include<stdio.h>
+#include "./Add/Add.h"
 extern void TestFun();
 int main(int argc,char **argv)
 {
     printf("Hello World\n");
     TestFun();
+    AddFunc();
     getchar();
     return 0;
 }
